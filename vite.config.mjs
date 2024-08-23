c911dbd2904f9bffd243d61ea4dac27b54b2c9d3
WoolawayWx <cade@woolawaywx.com>
2024-08-23 14:30:04 -0500

    Try 10

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,5 +10,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    historyApiFallback: true, // Ensures fallback to index.html for non-file requests
+  },
 })
