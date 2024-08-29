4d224b7f796c8713e9f7dddb1abea0e80a28889b
WoolawayWx <cade@woolawaywx.com>
2024-08-29 09:53:16 -0500

    Updated file names on the forecast maps

--- src/views/weather/forecastPlots.vue
+++ src/views/weather/forecastPlots.vue
@@ -4,8 +4,8 @@
         <p>Data is from the NDFD by NOAA.</p>
         <imageDis
             ImageURL="https://storage.cloud.google.com/weather-emergency-group-6a7c0.appspot.com/public/images/map_temp.png" />
+        <imageDis ImageURL="https://storage.cloud.google.com/weather-emergency-group-6a7c0.appspot.com/public/images/map_winds.png"/>
         <imageDis ImageURL="https://storage.cloud.google.com/weather-emergency-group-6a7c0.appspot.com/public/images/map_windgusts.png"/>
-        <imageDis ImageURL="https://storage.cloud.google.com/weather-emergency-group-6a7c0.appspot.com/public/images/map_windsgusts.png"/>
         <p class="text-black"><i>Images are property of WoolawayWx, and should be credited as such. Distribution without credit will result in a copyright notice.</i></p>
     </div>
 
