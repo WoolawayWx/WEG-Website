38dfca7ccc0a8a20d8b195c5607db3528c849bf8
WoolawayWx <cade@woolawaywx.com>
2024-08-28 21:57:46 -0500

    Updated homepage background to load faster, switched to local fonts, and removed unused files from the site.

--- src/components/Footer/appFooter.vue
+++ src/components/Footer/appFooter.vue
@@ -3,7 +3,7 @@
         <div class="bg-primary w-full">
             <div class="w-4/5 mx-[10%] py-4 md:flex-row flex flex-col">
                 <div class="md:w-1/3 flex justify-center items-center w-full md:min-w-1/3">
-                    <img src="@/assets/images/_branding/logo/WEG_White.svg" width="100%" height="100%" class="w-[30%] h-[30%] md:w-1/2 md:h-1/2">
+                    <img src="@/assets/images/_branding/logo/WEG_White.svg" width="100%" height="100%" class="w-[30%] h-[30%] md:w-1/2 md:h-1/2" alt="Logo for Weahter Emergency Group">
                 </div>
                 <div class="md:w-1/3 flex justify-center items-center w-full md:min-w-1/3">
                     <p class="text-white font-bold uppercase text-lg py-2"><i>Always Pushing for Better</i></p>
